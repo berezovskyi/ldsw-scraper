7a27fb63032bd4bd1b9d8e6a60a5b9fa728dc8b0
Andrew Berezovskyi <andriib@kth.se>
2024-10-07 23:21:15 +0200

    extract code into a script
    Signed-off-by: Andrew Berezovskyi <andriib@kth.se>

---
+++ bin/scrape.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# set -e
+set -uo pipefail
+# set -x
+
+# "Accept: text/turtle, application/trig, application/rdf+xml;q=0.9, application/ld+json;q=0.9, application/n-triples;q=0.5, application/n-quads;q=0.5"
+CURLOPT="-L"
+mkdir -p data/o/oslc/
+curl "https://open-services.net/ns/core" --header "Accept: text/turtle" $CURLOPT > data/o/oslc/core-vocab.ttl
+curl "https://open-services.net/ns/core" --header "Accept: application/rdf+xml, application/xml;q=0.1" $CURLOPT > data/o/oslc/core-vocab.rdf
+curl "https://open-services.net/ns/core" --header "Accept: application/n-triples" $CURLOPT > data/o/oslc/core-vocab.nt
+
+curl "http://open-services.net/ns/core/shapes/3.0" --header "Accept: text/turtle" $CURLOPT > data/o/oslc/core-shapes.ttl
+curl "http://open-services.net/ns/core/shapes/3.0" --header "Accept: application/rdf+xml, application/xml;q=0.1" $CURLOPT > data/o/oslc/core-shapes.rdf
+curl "http://open-services.net/ns/core/shapes/3.0" --header "Accept: application/n-triples" $CURLOPT > data/o/oslc/core-shapes.nt
