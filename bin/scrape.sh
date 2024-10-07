4922b36c35ea2baddfef5b8972a844ce212bd574
Andrew Berezovskyi <andriib@kth.se>
2024-10-08 00:20:05 +0200

    add HTML check
    Signed-off-by: Andrew Berezovskyi <andriib@kth.se>

--- bin/scrape.sh
+++ bin/scrape.sh
@@ -6,6 +6,24 @@ set -uo pipefail
 # "Accept: text/turtle, application/trig, application/rdf+xml;q=0.9, application/ld+json;q=0.9, application/n-triples;q=0.5, application/n-quads;q=0.5"
 CURLOPT="-L --fail --silent --show-error"
 
+function delete_if_html() {
+   file_path="$1"
+
+   mime_type=$(file --mime-type "$file_path" | awk '{print $2}')
+
+   # Check if the MIME type is text/html
+   if [ "$mime_type" = "text/html" ]; then
+      # Additional check for common HTML tags in the first 5 lines
+      if head -n 5 "$file_path" | grep -q "<html>" || head -n 5 "$file_path" | grep -q "<!DOCTYPE html>"; then
+         # Delete the file
+         rm "$file_path"
+         echo "File $file_path has been deleted because its MIME type is text/html and it contains HTML tags in the first 5 lines."
+      else
+         echo "File $file_path has a MIME type of text/html but does not contain common HTML tags in the first 5 lines. It will not be deleted."
+      fi
+   fi
+}
+
 function curl_try_all() {
    uri="$1"
    path="$2"
@@ -17,17 +35,23 @@ function curl_try_all() {
    echo -e "\nFetching ${path}"
 
    echo -e -n "\tTurtle   "
-   { curl "$uri" --header "Accept: text/turtle" $CURLOPT >"${outpath}.ttl" && echo "✅" ; } || { rm "${outpath}.ttl" ; }
+   { curl "$uri" --header "Accept: text/turtle" $CURLOPT >"${outpath}.ttl" && echo "✅"; } || { rm "${outpath}.ttl"; }
+   delete_if_html "${outpath}.ttl"
    echo -e -n "\tRDF/XML   "
-   { curl "$uri" --header "Accept: application/rdf+xml, application/xml;q=0.1" $CURLOPT >"${outpath}.rdf" && echo "✅" ; } || { rm "${outpath}.rdf" ; }
+   { curl "$uri" --header "Accept: application/rdf+xml, application/xml;q=0.1" $CURLOPT >"${outpath}.rdf" && echo "✅"; } || { rm "${outpath}.rdf"; }
+   delete_if_html "${outpath}.rdf"
    echo -e -n "\tN-Triples   "
-   { curl "$uri" --header "Accept: application/n-triples" $CURLOPT >"${outpath}.nt" && echo "✅" ; } || { rm "${outpath}.nt"  ; }
+   { curl "$uri" --header "Accept: application/n-triples" $CURLOPT >"${outpath}.nt" && echo "✅"; } || { rm "${outpath}.nt"; }
+   delete_if_html "${outpath}.nt"
    echo -e -n "\tN-Quads   "
-   { curl "$uri" --header "Accept: application/n-quads" $CURLOPT >"${outpath}.nq" && echo "✅" ; } || { rm "${outpath}.nq" ; }
+   { curl "$uri" --header "Accept: application/n-quads" $CURLOPT >"${outpath}.nq" && echo "✅"; } || { rm "${outpath}.nq"; }
+   delete_if_html "${outpath}.nq"
    echo -e -n "\tTriG   "
-   { curl "$uri" --header "Accept: application/trig" $CURLOPT >"${outpath}.trig" && echo "✅" ; } || { rm "${outpath}.trig" ; }
+   { curl "$uri" --header "Accept: application/trig" $CURLOPT >"${outpath}.trig" && echo "✅"; } || { rm "${outpath}.trig"; }
+   delete_if_html "${outpath}.trig"
    echo -e -n "\tJSON-LD   "
-   { curl "$uri" --header "Accept: application/ld+json" $CURLOPT >"${outpath}.jsonld" && echo "✅" ; } || { rm "${outpath}.jsonld" ; }
+   { curl "$uri" --header "Accept: application/ld+json" $CURLOPT >"${outpath}.jsonld" && echo "✅"; } || { rm "${outpath}.jsonld"; }
+   delete_if_html "${outpath}.jsonld"
 }
 
 function curl_try_exact() {
@@ -42,7 +66,8 @@ function curl_try_exact() {
    echo -e "\nFetching ${path}"
 
    echo -e -n "\t${accept}   "
-   { curl "$uri" --header "Accept: ${accept}" $CURLOPT >"${outpath}.ttl" && echo "✅" ; } || { rm "${outpath}.ttl" ; }
+   { curl "$uri" --header "Accept: ${accept}" $CURLOPT >"${outpath}" && echo "✅"; } || { rm "${outpath}"; }
+   delete_if_html "${outpath}"
 }
 
 # TODO: handle /-ending vocab without #
